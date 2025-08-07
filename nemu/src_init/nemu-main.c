edd884728a10ecc18e71f62943332568d2743270
tracer-ics2024 <tracer@njuics.org>
2025-08-07 12:28:18 +0000

    >  compile NEMU 231220000 张三 Linux yqiaoliang 6.11.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 16 14:19:41 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  12:28:18 up 1 day, 19:52,  7 users,  load average: 0.12, 0.05, 0.01

--- nemu/src_init/nemu-main.c
+++ nemu/src_init/nemu-main.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
+  printf("am_init_monitor finish");
 #else
   init_monitor(argc, argv);
 #endif
