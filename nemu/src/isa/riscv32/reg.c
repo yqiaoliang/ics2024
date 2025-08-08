d032781a8f18a90d807675d6c3d1f318f67efd10
tracer-ics2024 <tracer@njuics.org>
2025-08-08 09:51:22 +0000

    >  compile NEMU 231220000 张三 Linux yqiaoliang 6.11.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 16 14:19:41 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  09:51:22 up 2 days,  7:06,  7 users,  load average: 0.06, 0.03, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -34,7 +34,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   if (strcmp(s, "pc") == 0){
     return cpu.pc;
   }
-  for(int i = 0; i < 33; i++){
+  for(int i = 0; i < 32; i++){
     if (strcmp(s, regs[i])==0){
       * success = true;
       return cpu.gpr[i];
