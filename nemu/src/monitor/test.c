d0d7a4eeabdcb3bdf2ad157676793f7a3892ea53
tracer-ics2024 <tracer@njuics.org>
2025-08-16 02:51:31 +0000

    >  compile NEMU 231220000 张三 Linux yqiaoliang 6.11.0-8-generic #8-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 16 14:19:41 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  02:51:31 up 4 days, 19:58,  9 users,  load average: 0.04, 0.04, 0.00

--- nemu/src/monitor/test.c
+++ nemu/src/monitor/test.c
@@ -7,8 +7,8 @@
 int test(){
     for (int i = 0; i < 1; i++) {
         printf("test %d\n", i);
-        // char * this_expr = get_rand_expr();
-        char * this_expr = "(19+29)*(((35-72)))/83*66";
+        char * this_expr = get_rand_expr();
+        // char * this_expr = "(19+29)*(((35-72)))/83*66";
         // printf("expr: %s\n", this_expr);
         bool result = true;
         bool *success = &result;
