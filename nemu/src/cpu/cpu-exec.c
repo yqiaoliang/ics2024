e33a6616515dbeac83543415b8a3bdda2ef589c1
tracer-ics2024 <tracer@njuics.org>
2025-09-03 17:48:43 +0000

    >  compile NEMU 231220000 张三 Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  17:48:43 up 14:43,  6 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,12 +41,14 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   WP *head = get_wp_list();
+
+  char * nemu_state_names[] = {"NEMU_RUNNING", "NEMU_STOP", "NEMU_END", "NEMU_ABORT", "NEMU_QUIT"};
   while(head != NULL){
     bool success = true;
     int result = expr(head->expr, &success);
     if (result != head->expr_result) {
       if (nemu_state.state == NEMU_RUNNING) nemu_state.state = NEMU_STOP;
-      printf("The system triggered a monitoring point, current nemu state: %d \n", nemu_state.state);
+      printf("The system triggered a monitoring point, current nemu state: %s \n", nemu_state_names[nemu_state.state]);
       return;
     }
     head = head->next;
