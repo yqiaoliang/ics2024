c044971e75dfe286a2e13a7acfd2d268d2cc8ad6
tracer-ics2024 <tracer@njuics.org>
2025-09-03 13:24:04 +0000

    >  compile NEMU 231220000 张三 Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  13:24:04 up 11:01,  6 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -66,6 +66,7 @@ WP * new_wp(char *expr_) {
   wp->expr_result = expr(wp->expr, success);
   if (wp->expr == NULL || !result) {
     printf("Failed to allocate memory for watchpoint expression.\n");
+    head = wp;
     free_wp(wp->NO);
     return NULL;
   }
