e03fb3ed9331dc544b420ce610df7e6df585620f
tracer-ics2024 <tracer@njuics.org>
2025-09-07 15:42:34 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  15:42:34 up 2 days, 10:03,  6 users,  load average: 0.25, 0.07, 0.02

--- nemu/src/filelist.mk
+++ nemu/src/filelist.mk
@@ -17,6 +17,7 @@ SRCS-y += src/nemu-main.c
 DIRS-y += src/cpu src/monitor src/utils
 DIRS-$(CONFIG_MODE_SYSTEM) += src/memory
 DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
+$(info # CONFIG_TARGET_AM = $(CONFIG_TARGET_AM))
 
 SHARE = $(if $(CONFIG_TARGET_SHARE),1,0)
 # LIBS += $(if $(CONFIG_TARGET_NATIVE_ELF),-lreadline -ldl -pie,)
