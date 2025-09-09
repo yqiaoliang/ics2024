86eaee5fad5809ecbf2dac0af123daf4b081da07
tracer-ics2024 <tracer@njuics.org>
2025-09-09 03:50:21 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  03:50:21 up 3 days,  3:22,  7 users,  load average: 0.02, 0.06, 0.01

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -36,7 +36,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 run: run-env
 # 	$(call git_commit, "run NEMU")
 	$(info run command in native.mk)
-	(info NEMU_EXEC is $(NEMU_EXEC))
+	$(info NEMU_EXEC is $(NEMU_EXEC))
 	$(NEMU_EXEC)
 
 run-batch: run-env
