9cf108e550fc78dad7080bc81122b5f8a318320b
tracer-ics2024 <tracer@njuics.org>
2025-09-10 14:40:39 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  14:40:39 up 3 days, 21:43,  7 users,  load average: 0.16, 0.15, 0.06

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -100,7 +100,6 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
-  printf("test0\n");
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   char *iringbuf_p = iringbuf[iringbuf_index];
