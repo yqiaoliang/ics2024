3f5153d2bf0c5c9f5dd31b3d8cf3a291438d7c81
tracer-ics2024 <tracer@njuics.org>
2025-09-10 10:37:36 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  10:37:36 up 3 days, 19:05,  7 users,  load average: 0.14, 0.09, 0.05

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -38,7 +38,7 @@ void init_iringbuf(){
 
 void printf_iringbuf(){
   #ifdef CONFIG_ITRACE
-  printf("------- Instruction Trace Ring Buffer ------\n");
+    printf("------- Instruction Trace Ring Buffer ------\n");
     if (iringbuf_full){
       for (int i = iringbuf_index; i < iringbuf_len; i++){
         // if (i == iringbuf_len - 1) printf("ERROR: ");
@@ -52,6 +52,8 @@ void printf_iringbuf(){
       else printf("       ");
       printf("%s\n", iringbuf[i]);
     }
+
+    printf("--------------------------------------------\n");
   #endif
 }
 
