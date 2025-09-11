2c4fcb6ccd42de3a32be893549b1aa13dd4fdd0a
tracer-ics2024 <tracer@njuics.org>
2025-09-11 08:45:10 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  08:45:10 up 4 days,  4:41,  7 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -75,7 +75,7 @@ static int parse_args(int argc, char *argv[]) {
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
-    {"ftrace"   , required_argument, NULL, 'f'},
+    {"ftrace"   , required_argument, NULL, 256},
     {0          , 0                , NULL,  0 },
   };
   int o;
@@ -85,7 +85,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'f': init_ftrace(optarg);break;
+      case 256: init_ftrace(optarg);break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
