97ec07f9db007e3788ebc302d70bd171ef78ae3d
tracer-ics2024 <tracer@njuics.org>
2025-09-12 12:13:25 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  12:13:25 up 4 days, 18:20,  7 users,  load average: 0.17, 0.13, 0.10

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -18,7 +18,7 @@ int printf(const char *fmt, ...) {
     cur++;
   }
 
-  int d0 = va_arg(ap, int); d0+=1;
+  int d0 = va_arg(ap, int);
   char *s0 = va_arg(ap, char *); 
   char d1 = va_arg(ap, int); d1++;
   char *s1 = va_arg(ap, char *);
@@ -27,7 +27,11 @@ int printf(const char *fmt, ...) {
   char *s2 = va_arg(ap, char *);
   char d4 = va_arg(ap, int); d4++;
 
-  putch(all_num[1]);
+  while (d0){
+    putch(all_num[d0%10]);
+    d0 /= 10;
+  }
+  putch('\n');
 
   while(*s0++) putch(*s0);
   putch('\n');
