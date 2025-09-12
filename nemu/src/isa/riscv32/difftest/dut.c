f9e4f4bea34be96d449d1a5f06d2c3b3e9cd46cf
tracer-ics2024 <tracer@njuics.org>
2025-09-12 16:48:08 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  16:48:08 up 4 days, 22:40,  7 users,  load average: 0.03, 0.06, 0.02

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,7 +20,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < 32; i++) {
     if (ref_r->gpr[i] != gpr(i)) {
-      printf("ref_gpr of %s = %d, nemu gpr of %s = %d", reg_name(i), ref_r->gpr[i], reg_name(i), gpr(i));
+      printf("ref_gpr of %s = %d, nemu gpr of %s = %d\n", reg_name(i), ref_r->gpr[i], reg_name(i), gpr(i));
       return false;
     }
   }
