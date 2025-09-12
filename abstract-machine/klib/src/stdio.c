9c9e8eb51c9d8dbcb08b3bf5bcae2d81ca311df7
tracer-ics2024 <tracer@njuics.org>
2025-09-12 05:00:18 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  05:00:18 up 4 days, 13:56,  7 users,  load average: 0.55, 0.32, 0.12

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -69,19 +69,19 @@ int printf(const char *fmt, ...) {
     fmt++;
     switch(*fmt){
       case 'd':{
-        // int num = va_arg(ap, int);
-        // // char *buf = malloc(23* sizeof(char));
+        int num = va_arg(ap, int);
+        // char *buf = malloc(23* sizeof(char));
         
-        // char buf[23];
-        // memset(buf, '\0', 23);
-        // int_to_str(num, buf);
-        // int i = 0;
-        // while (buf[i] != '\0') {
-        //   putch(buf[i]);
-        //   i++;
-        //   count++;
-        // }
-        // // free(buf);
+        char buf[23];
+        memset(buf, '\0', 23);
+        int_to_str(num, buf);
+        int i = 0;
+        while (buf[i] != '\0') {
+          putch(buf[i]);
+          i++;
+          count++;
+        }
+        // free(buf);
         fmt++;
         break;
       }
