b7a6a5f647240d44810ee61a4c73356f63147e29
tracer-ics2024 <tracer@njuics.org>
2025-09-12 08:57:25 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  08:57:25 up 4 days, 16:23,  7 users,  load average: 0.11, 0.12, 0.10

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,46 +7,48 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  char this_buf[128];
-  char *out = this_buf;
-  va_list ap;
-  va_start(ap, fmt);
-
-  while(*fmt) {
-    if (*fmt != '%') {
-      *out++ = *fmt++;
-      continue;
-    }
-    fmt++;
-    switch(*fmt) {
-      case 'd':{
-        int num = va_arg(ap, int);
-        out = int_to_str(num, out);
-        fmt++;
-        break;
-      }
-      case 's':{
-        char *str = va_arg(ap, char *);
-        while(*str){
-          *out++ = *str++;
-        }
-        fmt++;
-        break;
-      }
-      case '%': {
-        *out++ = '%';
-        fmt++;
-        break;
-      }
-      default: fmt++; break;
-    }
-  }
-
-  while(*out) *out++ = '\0';
-
-  while(*out++) putch(*out); 
-
+  putch('-');
   return 0;
+  // char this_buf[128];
+  // char *out = this_buf;
+  // va_list ap;
+  // va_start(ap, fmt);
+
+  // while(*fmt) {
+  //   if (*fmt != '%') {
+  //     *out++ = *fmt++;
+  //     continue;
+  //   }
+  //   fmt++;
+  //   switch(*fmt) {
+  //     case 'd':{
+  //       int num = va_arg(ap, int);
+  //       out = int_to_str(num, out);
+  //       fmt++;
+  //       break;
+  //     }
+  //     case 's':{
+  //       char *str = va_arg(ap, char *);
+  //       while(*str){
+  //         *out++ = *str++;
+  //       }
+  //       fmt++;
+  //       break;
+  //     }
+  //     case '%': {
+  //       *out++ = '%';
+  //       fmt++;
+  //       break;
+  //     }
+  //     default: fmt++; break;
+  //   }
+  // }
+
+  // while(*out) *out++ = '\0';
+
+  // while(*out++) putch(*out); 
+
+  // return 0;
 
 }
 
