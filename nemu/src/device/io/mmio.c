9c247c6c94ddd46041413fd2c1a7994df9269b32
tracer-ics2024 <tracer@njuics.org>
2025-09-13 14:15:05 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  14:15:05 up 5 days,  8:12,  7 users,  load average: 0.11, 0.05, 0.05

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -55,7 +55,7 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-  printf("addr : 0x%0x", addr);
+  printf("addr : 0x%0x\n", addr);
   return map_read(addr, len, fetch_mmio_map(addr));
 }
 
