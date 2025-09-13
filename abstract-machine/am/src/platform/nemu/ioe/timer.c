dcb0daf5727c76fbc573f2b8bdebd2cc7bd6a9fa
tracer-ics2024 <tracer@njuics.org>
2025-09-13 06:58:50 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  06:58:50 up 5 days,  3:24,  7 users,  load average: 0.02, 0.02, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,6 +6,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = ((uint64_t)inl(RTC_ADDR) << 32) | inl(RTC_ADDR + 4);
+  // printf("uptime: %d", uptime->us);
   
 }
 
