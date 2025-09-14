1d46619bcd6d9dd93446c48b6abd8c16d6ebb1d7
tracer-ics2024 <tracer@njuics.org>
2025-09-14 17:23:15 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  17:23:15 up 5 days, 21:41,  7 users,  load average: 0.14, 0.33, 0.25

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -87,7 +87,7 @@ void init_vga() {
 #endif
 
   vmem = new_space(screen_size());
-  add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
+  add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), vga_update_screen);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
   IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
 }
