dfe8f675d8064609a69e199fe881cae3a28c3266
tracer-ics2024 <tracer@njuics.org>
2025-09-14 06:48:36 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  06:48:36 up 5 days, 14:45,  7 users,  load average: 0.05, 0.05, 0.00

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -77,6 +77,10 @@ void init_device() {
   IFDEF(CONFIG_TARGET_AM, ioe_init());
   init_map();
 
+  #ifdef CONFIG_HAS_VGA
+    printf("test0\n");
+  #endif
+
   IFDEF(CONFIG_HAS_SERIAL, init_serial());
   IFDEF(CONFIG_HAS_TIMER, init_timer());
   IFDEF(CONFIG_HAS_VGA, init_vga());
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -42,7 +42,6 @@ static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 
 static void init_screen() {
-  printf("test0\n");
   SDL_Window *window = NULL;
   char title[128];
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
