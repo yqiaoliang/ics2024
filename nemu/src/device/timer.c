8d010ebb3715df504ec322ffffc669f8a963504b
tracer-ics2024 <tracer@njuics.org>
2025-09-14 11:37:59 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  11:37:59 up 5 days, 17:44,  7 users,  load average: 0.10, 0.12, 0.04

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -23,13 +23,11 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
   
   if (!is_write && offset == 4) {
-    printf("enter rtc_io_handler 4\n");
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
   }
   else if (!is_write && offset == 0) {
-    printf("enter rtc_io_handler 0\n");
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
   }
