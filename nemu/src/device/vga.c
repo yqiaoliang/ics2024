f6c1138dba2cb064ceffdbf5ed285b4287d1056f
tracer-ics2024 <tracer@njuics.org>
2025-09-14 13:43:53 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  13:43:53 up 5 days, 18:50,  7 users,  load average: 0.16, 0.04, 0.01

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -71,9 +71,9 @@ static inline void update_screen() {
 #endif
 #endif
 
-void vga_update_screen() {
+void vga_update_screen(uint32_t offset, int len, bool is_write) {
   // #define SYNC_ADDR (VGACTL_ADDR + 4)
-  // update_screen();
+  update_screen();
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 }
@@ -94,7 +94,7 @@ void init_vga() {
 #endif
 
   vmem = new_space(screen_size());
-  add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
+  add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), vga_update_screen);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
   IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
 }
