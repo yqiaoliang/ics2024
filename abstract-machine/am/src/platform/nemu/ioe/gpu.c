9ad50c097e4f7fd01acbf5ca18cab26b0d7e3793
tracer-ics2024 <tracer@njuics.org>
2025-09-14 14:39:11 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  14:39:11 up 5 days, 19:16,  7 users,  load average: 0.08, 0.03, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -19,7 +19,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = GPU_WIDTH, .height = GPU_HEIGHT,
-    .vmemsz = 0
+    .vmemsz = GPU_HEIGHT * GPU_WIDTH * 32
   };
 }
 
@@ -41,7 +41,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     for (int col = 0; col < ctl->w; col++) {
       int fb_x = ctl->x + col;
       int fb_y = ctl->y + row;
-      int fb_index = fb_y * fb_width + fb_x;
+      int fb_index = fb_x * fb_width + fb_y;
 
       uint32_t addr = FB_ADDR + fb_index * 4;
 
