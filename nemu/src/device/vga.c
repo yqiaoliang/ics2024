ac6374150095cfdadb3f57ce45d72aa3f7699c28
tracer-ics2024 <tracer@njuics.org>
2025-09-14 14:43:56 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  14:43:56 up 5 days, 19:21,  7 users,  load average: 0.31, 0.35, 0.21

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -73,6 +73,14 @@ static inline void update_screen() {
 
 void vga_update_screen(uint32_t offset, int len, bool is_write) {
   // #define SYNC_ADDR (VGACTL_ADDR + 4)
+  printf("0: %d\n", vgactl_port_base[0]);
+  printf("1: %d\n", vgactl_port_base[1]);
+  printf("2: %d\n", vgactl_port_base[2]);
+  printf("3: %d\n", vgactl_port_base[3]);
+  printf("4: %d\n", vgactl_port_base[4]);
+  printf("5: %d\n", vgactl_port_base[5]);
+  printf("6: %d\n", vgactl_port_base[6]);
+  printf("7: %d\n", vgactl_port_base[7]);
   if (vgactl_port_base[4] | 1) update_screen();
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
@@ -90,7 +98,7 @@ void init_vga() {
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
-  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, vga_update_screen);
+  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
 #endif
 
   vmem = new_space(screen_size());
