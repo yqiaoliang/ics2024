013660cdd81fa55813e3ead834926aa526d9746c
tracer-ics2024 <tracer@njuics.org>
2025-09-14 13:22:27 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  13:22:27 up 5 days, 18:28,  7 users,  load average: 0.10, 0.06, 0.01

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -72,7 +72,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
-  printf("map_write\n");
+  printf(" data: %0x", data);
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
