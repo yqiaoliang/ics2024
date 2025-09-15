520474abd66c2d48bc2282ad2965fc2648cbb433
tracer-ics2024 <tracer@njuics.org>
2025-09-15 14:52:31 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  14:52:31 up 6 days,  5:34,  7 users,  load average: 0.09, 0.05, 0.01

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -16,11 +16,12 @@
 #include <isa.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
+  cpu.csr.mepc = epc;
+  cpu.csr.mcause = NO;
+  return cpu.csr.mtvec;
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
-  return 0;
 }
 
 word_t isa_query_intr() {
