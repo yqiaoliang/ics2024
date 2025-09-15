b87fec197ec69010ecd6b43073d2ef2414e3aaa4
tracer-ics2024 <tracer@njuics.org>
2025-09-15 15:41:18 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  15:41:18 up 6 days,  6:21,  7 users,  load average: 0.00, 0.03, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -157,7 +157,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(0x0B, s->pc));
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, word_t *csr_ptr = csr_read(imm); R(rd) = *csr_ptr; *csr_ptr = src1);
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, word_t *csr_ptr = csr_read(imm); R(rd) = *csr_ptr; *csr_ptr |= src1;);
-  // INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = csr_read(CSR_MEPC); MRET);
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = *csr_read(MEPC));
   
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
