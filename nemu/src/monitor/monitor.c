4abd73640ebe8744b7efceefd8b60de4bc68843e
tracer-ics2024 <tracer@njuics.org>
2025-09-19 08:35:57 +0000

    >  compile NEMU 2401210500 yqiaoliang Linux yqiaoliang 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:32:41 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  08:35:57 up 7 days, 20:41,  7 users,  load average: 0.07, 0.08, 0.09

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -110,7 +110,10 @@ void init_monitor(int argc, char *argv[]) {
   /* Set random seed. */
   init_rand();
 
-  init_trace();
+  // init_trace();
+  #ifdef CONFIG_ITRACE
+    init_iringbuf();
+  #endif
 
   /* Open the log file. */
   init_log(log_file);
